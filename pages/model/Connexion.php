451398dc200c0173dff7344184b49c6f2df16482
Pierre Emmanuel <80822446+Pierre501@users.noreply.github.com>
2022-05-17 17:12:04 +0200

    modif
    modif

--- pages/model/Connexion.php
+++ pages/model/Connexion.php
@@ -3,10 +3,10 @@ class Connexion
 {
     public function connexion()
     {
-        $host = 'localhost';
-    	$dbname = 'rechauffement_climatique';
-   		$username = 'admin';
-    	$password = 'admin';
+        $host = 'ec2-3-228-235-79.compute-1.amazonaws.com';
+    	$dbname = 'deacl2fvao6jda';
+   	$username = 'yhcvstnalxhowe';
+    	$password = '1ed2168f01fb2d8a116f258ed4bfa9c87ae1f23b0f568b007eee0dd0e2084805';
         $dsn = "host=$host port=5432 dbname=$dbname user=$username password=$password";
         $connexion = pg_connect($dsn);
 		return $connexion;
